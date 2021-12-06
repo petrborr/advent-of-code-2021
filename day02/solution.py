f612e0b6cef420793fbf6711e1c63ff4be981816
petrborr <petrborr@gmail.com>
2021-12-06 23:16:56 +0200

    day 2

---
+++ day02/solution.py
@@ -0,0 +1,37 @@
+def solution1(commands):
+    horizontal_position = 0
+    depth = 0
+    for command in commands:
+        direction, distance = command[0], int(command[1])
+        if direction == 'forward':
+            horizontal_position += distance
+        elif direction == 'up':
+            depth -= distance
+        elif direction == 'down':
+            depth += distance
+
+    return horizontal_position * depth
+
+def solution2(commands):
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+    for command in commands:
+        direction, distance = command[0], int(command[1])
+        if direction == 'forward':
+            horizontal_position += distance
+            depth += aim * distance
+        elif direction == 'up':
+            aim -= distance
+        elif direction == 'down':
+            aim += distance
+
+    return horizontal_position * depth
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        lines = [x for x in f.read().splitlines()]
+        commands = [line.split() for line in lines]
+        print(solution1(commands))
+        print(solution2(commands))
